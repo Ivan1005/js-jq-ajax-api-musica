841aa978a697cbb53922f556d4ea2f6e409cfb9f
Ivan <matteraivam@gmail.com>
2020-08-31 10:12:52 +0200

    correzione in classe con metodo più veloce

--- js/script.js
+++ js/script.js
@@ -17,34 +17,13 @@ $(document).ready(function() {
   var source = $('#entry-template').html();
   var template = Handlebars.compile(source);
   $("#select").change(function() {
-    var period = $("#select").val();
+    var period = $(this).val();
     console.log(period);
-    if (period == "pop") {
-      $('.Rock').hide();
-      $('.Metal').hide();
-      $('.Jazz').hide();
-      $('.Pop').show();
-    } else if (period == "rock") {
-      $('.Pop').hide();
-      $('.Metal').hide();
-      $('.Jazz').hide();
-      $('.Rock').show();
-    } else if (period == "metal") {
-      $('.Pop').hide();
-      $('.Rock').hide();
-      $('.Jazz').hide();
-      $('.Metal').show();
-    } else if (period == "jazz") {
-      $('.Pop').hide();
-      $('.Metal').hide();
-      $('.Jazz').show();
-      $('.Rock').hide();
-    } else {
-      $('.cd').show();
+    if ( period == "all") {
+      $(".cd").show();
+    }else {
+      $(".cd").hide();
+      $(".cd." + period).show();
     }
-
-
-
   });
-
 });
